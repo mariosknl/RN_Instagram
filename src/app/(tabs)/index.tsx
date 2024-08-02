2ac923fad765eaa80cdbd2f0b0e48d742435775a
mariosknl <mariosknl@outlook.com>
2024-08-02 12:14:54 +0300

    building the new post screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, View } from "react-native";
+import { FlatList } from "react-native";
 import posts from "~/assets/data/posts.json";
 import PostListItem from "~/src/components/PostListItem";
 
@@ -8,7 +8,12 @@ export default function FeedScreen() {
 			data={posts}
 			renderItem={({ item }) => <PostListItem post={item} />}
 			keyExtractor={(item) => item.id}
-			contentContainerStyle={{ gap: 10 }}
+			contentContainerStyle={{
+				gap: 10,
+				maxWidth: 512,
+				width: "100%",
+				alignSelf: "center",
+			}}
 			showsVerticalScrollIndicator={false}
 		/>
 	);
